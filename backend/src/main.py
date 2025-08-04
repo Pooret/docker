59ad1516fc426bd0aaf12cdec2e077251ab6e008
pooret-animl <tyler.poore@animl.health>
2025-08-04 14:59:25 -0400

    updated main for api key error

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 API_KEY = os.environ.get("API_KEY")
 MY_PROJECT = os.environ.get("MY_PROJECT")
 
+if not API_KEY:
+    raise NotImplementedError("'API_KEY' was not set")
+
 @app.get("/")
 def read_index():
     return {"hello":"world again!", "project_name":MY_PROJECT}
